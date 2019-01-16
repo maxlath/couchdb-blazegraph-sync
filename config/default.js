247a85b069b619bb6bae082b74d7ea9863b15896
maxlath <k@maxlath.eu>
2019-01-16 23:13:53 +0100

    first attempt

---
+++ config/default.js
@@ -0,0 +1,15 @@
+module.exports = {
+  couchdb: {
+    host: 'http://username:password@localhost:5984',
+    database: 'my-db'
+  },
+  blazegraph: {
+    host: 'http://localhost:8080',
+    namespace: 'kb'
+  },
+  serializer: {
+    workingDir: '/path/to/ttl_serializer/working_dir',
+    path: '/path/to/ttl_serializer.js'
+  },
+  buildUri: doc => `http://inventaire.io/entity/${doc._id}`
+}
---
+++ index.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+const { couchdb } = require('config')
+const follow = require('follow')
+const { red, green } = require('chalk')
+const db = `${couchdb.host}/${couchdb.database}`
+const update = require('./lib/update')
+
+const filterOutDesignDocs = doc => !doc._id.match(/^_design\//)
+
+follow({ db, filter: filterOutDesignDocs, include_docs: true }, (err, change) => {
+  if (err) return console.error(red('follow error'), err)
+
+  update(change)
+  .then(console.log.bind(null, green('RES')))
+  .catch(console.error.bind(null, red('ERR')))
+})
