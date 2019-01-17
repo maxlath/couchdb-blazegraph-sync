e363c7cdc86ff865035031289b99ca6acd91a554
maxlath <k@maxlath.eu>
2019-01-17 14:17:32 +0100

    skip changes below the last known seq

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 #!/usr/bin/env node
 const { couchdb } = require('config')
 const follow = require('follow')
-const { red, green } = require('chalk')
+const { red, green, grey } = require('chalk')
 const db = `${couchdb.host}/${couchdb.database}`
 const update = require('./lib/update')
+const lastSeq = require('./lib/last_seq')
 
 const filterOutDesignDocs = doc => !doc._id.match(/^_design\//)
 
 follow({ db, filter: filterOutDesignDocs, include_docs: true }, (err, change) => {
   if (err) return console.error(red('follow error'), err)
+
+  if (change.seq <= lastSeq.get()) {
+    console.log(grey('change is before last known seq, passing'), change.seq)
+    return
+  }
+
+  lastSeq.update(change.seq)
 
   update(change)
   .get('body')
