f26cfcbd4b8de06d1ae5b3d2e77a758588c4785b
maxlath <k@maxlath.eu>
2019-05-13 13:30:46 +0200

    update in bulk by aggregating changes before posting

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 #!/usr/bin/env node
-const { red } = require('chalk')
+const { red, yellow } = require('chalk')
 const configs = require('./lib/configs')
 const waitForDatabases = require('./lib/wait_for_databases')
 const followChanges = require('./lib/follow_changes')
 
-const init = (name, config) => {
+const init = config => {
   waitForDatabases(config)
-  .then(() => followChanges(name, config))
-  .catch(console.error.bind(null, red(`${name} init error`)))
+  .then(() => followChanges(config))
+  .catch(console.error.bind(null, red(`${config.name} init error`)))
 }
 
 Object.keys(configs).forEach(name => {
   const config = configs[name]
-  init(name, config)
+  if (config.disabled) return console.log(yellow(`${name} is disabled`))
+  config.name = name
+  init(config)
 })
